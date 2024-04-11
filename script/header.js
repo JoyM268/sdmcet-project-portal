442125098761d71df00efcc78024fc01c6711799
JoyM268 <125041935+JoyM268@users.noreply.github.com>
2024-04-11 14:32:24 +0530

    Add Dropdown Menu

---
+++ script/header.js
@@ -0,0 +1,39 @@
+function handleMenuDisplay() {
+    if (window.innerWidth < 400) {
+        document.getElementById('menu-click').style.display = "flex";
+    } else {
+        document.getElementById('menu-click').style.display = "none";
+    }
+}
+
+handleMenuDisplay();
+window.addEventListener('resize', function() {
+    if(document.getElementById('menu-drop').style.display != "flex"){
+        handleMenuDisplay();
+    }else if(document.getElementById('menu-drop').style.display == "flex" && window.innerWidth > 400){
+        document.getElementById('close-click').style.display = "none";
+        document.getElementById('menu-drop').style.display = "none";
+    }
+});
+
+function disableScroll(){
+    document.body.classList.add("stop-scrolling");
+}
+
+function enableScroll(){
+    document.body.classList.remove("stop-scrolling");
+}
+
+function openSlideMenu(){
+    document.getElementById('menu-drop').style.display = "flex";
+    document.getElementById('menu-click').style.display = "none";
+    document.getElementById('close-click').style.display = "flex";
+    disableScroll();
+}
+
+function closeSlideMenu(){
+    document.getElementById('menu-drop').style.display = "none";
+    document.getElementById('menu-click').style.display = "flex";
+    document.getElementById('close-click').style.display = "none";
+    enableScroll();
+}
