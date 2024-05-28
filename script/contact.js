17e8b0586a3f5ecff4da3598fe146d82e6ee163d
JoyM268 <125041935+JoyM268@users.noreply.github.com>
2024-05-28 19:18:49 +0530

    Contact Validation Update

--- script/contact.js
+++ script/contact.js
@@ -1,39 +1,49 @@
 function validatePage()
 {
-    let firstName = document.getElementById("fname").value;
-    let lastName = document.getElementById("lname").value;
-    let email = document.getElementById("email").value;
-    let phno = document.getElementById("phno").value;
-    let message = document.getElementById("message").value;
+    let firstName = document.getElementById("fname").value.trim();
+    let lastName = document.getElementById("lname").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let phno = document.getElementById("phno").value.trim();
+    let message = document.getElementById("message").value.trim();
     
     let namePattern = /^[a-zA-Z]+$/;
     let emailPattern =  /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    let phonePattern = /^[0-9]+$/;
+    let phonePattern = /^[0-9]{10}$/;
     let messagePattern = /[a-zA-Z]/;
 
-    if(!firstName.match(namePattern)){
-        alert("The first name should only contain alphabets.");
+    if(firstName == ""){
+        alert("Please enter the first name.");
+        return false;
+    }else if(!firstName.match(namePattern)){
+        alert("The first name should only include alphabets.");
         return false;
     }
 
-    if(!lastName.match(namePattern)){
-        alert("The last name should only contain alphabets.");
+    if(!lastName.match(namePattern) && lastName != ""){
+        alert("The last name should only include alphabets.");
         return false;
     }
 
-    if(!email.match(emailPattern)){
-        alert("Enter a valid Email");
+    if(email == ""){
+        alert("Please enter your email address.");
+        return false;
+    }else if(!email.match(emailPattern)){
+        alert("Please enter a valid email address.");
         return false;
     }
 
-    if(!phno.match(phonePattern)){
-        alert("Enter a valid phone number");
+    if(phno == ""){
+        alert("Please enter your phone number.");
+        return false;
+    }else if(!phno.match(phonePattern)){
+        alert("Please enter a valid phone number of 10 digits.");
         return false;
     }
     
     if(!message.match(messagePattern)){
-        alert("Message is required");
+        alert("Please enter your message.");
         return false;
     }
+
     return true;
 }
