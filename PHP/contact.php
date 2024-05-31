ad808421c96ccf88eca6631edcc89a9029254d15
JoyM268 <125041935+JoyM268@users.noreply.github.com>
2024-05-31 08:44:20 +0530

    Add Contact Data Storage

---
+++ PHP/contact.php
@@ -0,0 +1,26 @@
+<?php
+    $fName = $_POST["fname"];
+    $lName = $_POST["lname"];
+    $email = $_POST["email"];
+    $phno = $_POST["phno"];
+    $message = $_POST["message"];
+
+    $database = "ProjectPortal";
+    $url = "localhost";
+    $usrname = "root";
+    $password = "";
+    
+    $conn = new mysqli($url, $usrname, $password, $database);
+    if($conn->connect_error){
+        echo "Connection Failed ". $conn->connect_error;
+    }
+
+    $sql = "INSERT INTO ContactData (First_Name, Last_Name, Email, Phone_No, Message) VALUES ('$fName', '$lName', '$email', $phno, '$message')";
+    if($conn->query($sql) == TRUE){
+        echo "The message was submitted successfully";
+    }else{
+        echo "The message submission failed";
+    }
+
+    $conn->close();
+?>
