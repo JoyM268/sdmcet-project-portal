ffd7ed30d5a965f67c60b35aab6b65d89d404dae
JoyM268 <125041935+JoyM268@users.noreply.github.com>
2024-06-09 23:23:11 +0530

    Menu Animation Update

--- script/header.js
+++ script/header.js
@@ -8,11 +8,12 @@ function handleMenuDisplay() {
 
 handleMenuDisplay();
 window.addEventListener('resize', function() {
-    if(document.getElementById('menu-drop').style.display != "flex"){
+    if(document.getElementById('menu-drop').classList.contains("hide")){
         handleMenuDisplay();
-    }else if(document.getElementById('menu-drop').style.display == "flex" && window.innerWidth > 480){
+    }else if(document.getElementById('menu-drop').classList.contains("show") && window.innerWidth > 480){
         document.getElementById('close-click').style.display = "none";
-        document.getElementById('menu-drop').style.display = "none";
+        document.getElementById('menu-drop').classList.add("hide");
+        document.getElementById('menu-drop').classList.remove("show");
     }
 });
 
@@ -25,14 +26,16 @@ function enableScroll(){
 }
 
 function openSlideMenu(){
-    document.getElementById('menu-drop').style.display = "flex";
+    document.getElementById('menu-drop').classList.add("show");
+    document.getElementById('menu-drop').classList.remove("hide");
     document.getElementById('menu-click').style.display = "none";
     document.getElementById('close-click').style.display = "flex";
     disableScroll();
 }
 
 function closeSlideMenu(){
-    document.getElementById('menu-drop').style.display = "none";
+    document.getElementById('menu-drop').classList.add("hide");
+    document.getElementById('menu-drop').classList.remove("show");
     document.getElementById('menu-click').style.display = "flex";
     document.getElementById('close-click').style.display = "none";
     enableScroll();
